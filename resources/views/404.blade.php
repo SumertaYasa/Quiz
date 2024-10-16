132be5ad5705986c2d6c0e0513fad4d9ae2d80cc
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-10-16 13:12:23 +0800

    update page 404.blade.php

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -6,14 +6,10 @@
     <title>404 - Halaman Tidak Ditemukan</title>
     @vite('resources/css/app.css')
 </head>
-<body class="bg-gray-100 h-screen flex items-center justify-center">
-    <div class="max-w-md w-full bg-white shadow-lg rounded-lg p-8 text-center">
-        <h1 class="text-4xl font-bold text-gray-800 mb-4">404</h1>
-        <p class="text-xl text-gray-600 mb-8">Ups! Halaman tidak ditemukan.</p>
-        <p class="text-gray-500 mb-8">Halaman yang Anda cari mungkin telah dihapus, diganti namanya, atau sementara tidak tersedia.</p>
-        <a href="{{ url('/') }}" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300">
-            Kembali ke Beranda
-        </a>
+<body class="bg-gray-200 h-screen flex items-center justify-center">
+    <div class="max-w-md w-full  text-center">
+        <img src="/img/404.png" alt="">
+        <a href="/" class="px-6 py-2.5 text-white transition duration-300 bg-yellow-400 rounded-md hover:bg-yellow-600 font-bold">Beranda</a>
     </div>
 </body>
 </html>
